264ef4600287be633d7a67797cf159d8803e1d11
Aiden <tyjk32@gmail.com>
2021-07-22 00:57:40 +0900

    ModelMapper 적용

--- src/main/java/com/studyolle/settings/SettingsController.java
+++ src/main/java/com/studyolle/settings/SettingsController.java
@@ -4,6 +4,7 @@ import com.studyolle.account.AccountService;
 import com.studyolle.account.CurrentUser;
 import com.studyolle.domain.Account;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -20,16 +21,12 @@ import javax.validation.Valid;
 public class SettingsController {
 
     private final AccountService accountService;
-
-    @InitBinder("passwordForm")
-    public void initBinder(WebDataBinder webDataBinder) {
-        webDataBinder.addValidators(new PasswordFormValidator());
-    }
+    private final ModelMapper modelMapper;
 
     @GetMapping("/settings/profile")
     public String UpdateProfileForm(@CurrentUser Account account, Model model) {
         model.addAttribute(account);
-        model.addAttribute(new Profile(account));
+        model.addAttribute(modelMapper.map(account, Profile.class));
         return "settings/profile";
     }
 
@@ -68,7 +65,7 @@ public class SettingsController {
     @GetMapping("/settings/notifications")
     public String updateNotificationsForm(@CurrentUser Account account, Model model) {
         model.addAttribute(account);
-        model.addAttribute(new Notifications(account));
+        model.addAttribute(modelMapper.map(account, Notifications.class));
         return "settings/notifications";
     }
 
